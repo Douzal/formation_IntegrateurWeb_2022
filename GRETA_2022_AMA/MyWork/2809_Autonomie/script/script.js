967e5e43704328a15d2d49212ff2395c07d875f6
Douzal <Alexis_Masson_@hotmail.fr>
2022-09-27 21:27:22 +0200

    ajout du J2 et début travail J3

---
+++ GRETA_2022_AMA/MyWork/2809_Autonomie/script/script.js
@@ -0,0 +1,45 @@
+$(function() {
+    'use strict';
+    // console.warn('jquery ready');
+
+    // set the good section visible
+
+    /* FUNCTIONS */   
+    let init = function () {
+        bindClick();
+    };
+
+    function makeVisible(id) {
+        let allNames = ['Home', 'Blog', 'Add', 'About'];
+        let clickedPage = allNames.indexOf(id);
+        console.group();
+        console.log(`clickedPage : ${clickedPage}`);
+
+        for(let nomPage of allNames) {
+            // set to hidden
+            $("#"+nomPage).css("display", "none");
+            
+            // console.log(`nomPage : ${nomPage}\nallNames[clickedPage] : ${allNames[clickedPage]}`);
+            // console.warn(nomPage === allNames[clickedPage]);
+            if(nomPage === allNames[clickedPage]) {
+
+                $("#"+nomPage).css("display", "block");
+            }
+        }
+        console.groupEnd();
+    }
+
+    let bindClick = function () {
+        $('#nav a').click(function(e) {
+            // get the a clicked
+            let id = $(this).attr("id");
+            id = id.substring(1);
+            // console.log(`id : ${id}`);
+            makeVisible(id);
+
+        });
+    }
+
+    init();
+
+});
---
+++ GRETA_2022_AMA/MyWork/script/script.js
@@ -0,0 +1,9 @@
+$(function() {
+    console.warn('hey js');
+
+    // create menu
+    $('#menu').append('<a href="1-rappels.html">Page 1 : 1-rappels.html</a>').append('<br />')
+    .append('<a href="2-containers.html">Page 2 : 2-containers.html</a><br />')
+    .append('<a href="4-menu-navigation.html">Page 4 : 4-menu-navigation.html</a><br />')
+    .append('<a href="5-multimedia.html">Page 5 : 5-multimedia.html</a><br />');
+})
